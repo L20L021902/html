01def00fefcad5de88a91757173315370988001e
Salikhov Daniel <salihov_daniel@mail.ru>
2022-12-29 22:29:56 +0300

    release v7

--- source/scriptEmployee.js
+++ source/scriptEmployee.js
@@ -51,44 +51,19 @@ function clearHighlight() {
    })
 }
 
-// // addBtn
 
+//ADDBTN INPUT
 
 let addBtn = document.querySelector('.addBtn');
 let rowsTbody = document.querySelector('tbody');
 let cellsTbody = document.querySelectorAll('tbody td');
 let bodyRows = document.querySelectorAll('.rowsonee td');
-// console.log(bodyRows);
 
 addBtn.addEventListener('click', () => {
-   // let serialNumber = +(rowsTbody.lastElementChild.children[0].innerHTML) + 1;
-   // let productNumber = document.querySelector('.zero-input').value;
-   // let productName = document.querySelector('.one-input').value;
-   // let productCategory = document.querySelector('.two-input').value;
-   // let productFirstPrice = document.querySelector('.three-input').value;
-   // let productSecondPrice = document.querySelector('.four-input').value;
-
 
    addRow();
 
 });
-
-// let newTr = document.createElement('tr');
-
-// for (let i = 0; i < bodyRows.length; i++) {
-//    let newTd = document.createElement('td');
-//    newTd.innerHTML = `${i}`;
-//    newTr.append(newTd);
-// }
-
-// let rowsTr = rowsTbody.append(newTr);
-
-
-
-
-
-
-
 
 
 function addRow() {
@@ -99,73 +74,116 @@ function addRow() {
    let productCategory = document.querySelector('.two-input').value;
    let productFirstPrice = document.querySelector('.three-input').value;
    let productSecondPrice = document.querySelector('.four-input').value;
-   let cellsBtn = document.querySelector('.cells-btn');
-
-   let newTr = document.createElement('tr');
-   rowsTbody.append(newTr);
-
-   // for (let i = 0; i < bodyRows.length; i++) {
-   //    let newTd = document.createElement('td');
-   // newTd.innerHTML = `${productName}`;
-   //    newTr.appendChild(newTd);
-   // }
-
-   // let rowsTr = rowsTbody.append(newTr);
-
-   let td1 = document.createElement('td');
-   td1.innerHTML = `${serialNumber}`;
-
-   let td2 = document.createElement('td');
-   td2.innerHTML = `${productNumber}`;
-
-   let td3 = document.createElement('td');
-   td3.innerHTML = `${productName}`;
-
-   let td4 = document.createElement('td');
-   td4.innerHTML = `${productCategory}`;
-
-   let td5 = document.createElement('td');
-   td5.innerHTML = `${productFirstPrice}`;
-
-   let td6 = document.createElement('td');
-   td6.innerHTML = `${productSecondPrice}`;
-
-   // TD STATUS
-   // function arrStatus(arr){
-   //    return splice([Math.floor(Math.random()*arr.length)],1);
-   // }
-
-   // let arrStatus = ['却贷','有货'];
-
-   // let td7 = document.createElement('td');
-
-   //TD LINK
-   td9 = document.createElement('td');
-
-   td9.classList.add('cells-btn');
-
-   // let linkOne = td9.createElement('a');
-   // linkOne.innerHTML = '编辑';
-   // linkOne.classList.add('cells-btn1');
-   // td9.append(linkOne)
-
-   // let linkTwo = td9.createElement('a');
-   // linkTwo.innerHTML = '编辑';
-   // linkTwo.classList.add('cells-btn2');
-   // td9.append(linkTwo);
 
 
 
-   // td9.innerHTML = `${cellsBtn}`;
+   if (productNumber === '' || productName === '' || productName === '' || productCategory === '' || productFirstPrice === '' || productSecondPrice === '') {
+      return;
+   } else {
 
-   newTr.appendChild(td1);
-   newTr.appendChild(td2);
-   newTr.appendChild(td3);
-   newTr.appendChild(td4);
-   newTr.appendChild(td5);
-   newTr.appendChild(td6);
-   newTr.appendChild(td9);
+      //INPUT
+      let newTr = document.createElement('tr');
+      rowsTbody.append(newTr);
 
+      let td1 = document.createElement('td');
+      td1.innerHTML = `${serialNumber}`;
+
+      let td2 = document.createElement('td');
+      td2.innerHTML = `${productNumber}`;
+
+      let td3 = document.createElement('td');
+      td3.innerHTML = `${productName}`;
+
+      let td4 = document.createElement('td');
+      td4.innerHTML = `${productCategory}`;
+
+      let td5 = document.createElement('td');
+      td5.innerHTML = `${productFirstPrice}`;
+
+      let td6 = document.createElement('td');
+      td6.innerHTML = `${productSecondPrice}`;
+
+      // TD STATUS
+      let td7 = document.createElement('td');
+
+      let words = ['却贷', '有货'];
+      let wordArr = words[Math.floor(Math.random() * words.length)];
+
+      if (wordArr === '却贷') {
+         td7.classList.add('cells-status1');
+      } else {
+         td7.classList.add('cells-status2');
+      }
+
+      td7.innerHTML = `${wordArr}`;
+
+      //TD TIME
+
+      let td8 = document.createElement('td');
+
+      function randomDate(start, end) {
+         return new Date(start.getTime()
+            + Math.random() * (end.getTime() - start.getTime()));
+      }
+
+      var myDate = randomDate(new Date(2012, 0, 1), new Date());
+
+      let timeData = myDate.getFullYear() + '-'
+         + ('0' + (myDate.getMonth() + 1)).slice(-2)
+         + '-' + ('0' + myDate.getDate()).slice(-2)
+         + ' '
+         + ('0' + myDate.getHours()).slice(-2)
+         + ':' + ('0' + myDate.getMinutes()).slice(-2)
+         + ':' + ('0' + myDate.getSeconds()).slice(-2)
+         ;
+
+      td8.innerHTML = `${timeData}`;
+
+      //TD LINK
+      td9 = document.createElement('td');
+      td9.classList.add('cells-btn');
+
+      let linkOne = document.createElement('a');
+      let linkAppendOne = td9.appendChild(linkOne);
+      linkAppendOne.classList.add('cells-btn1');
+      linkAppendOne.innerHTML = '编辑';
+
+      let linkTwo = document.createElement('a');
+      let linkAppendTwo = td9.appendChild(linkTwo);
+      linkAppendTwo.classList.add('cells-btn2');
+      linkAppendTwo.innerHTML = '册除';
+
+      newTr.appendChild(td1);
+      newTr.appendChild(td2);
+      newTr.appendChild(td3);
+      newTr.appendChild(td4);
+      newTr.appendChild(td5);
+      newTr.appendChild(td6);
+      newTr.appendChild(td7);
+      newTr.appendChild(td8);
+      newTr.appendChild(td9);
+
+      //CELLS-LINK IN INPUT
+      // let btnEdit = document.querySelector('.cells-btn1');
+      let boxModal = document.querySelector('.box');
+
+      linkAppendOne.addEventListener('click', (event) => {
+         boxModal.style.display = 'flex';
+         event.preventDefault();
+      });
+
+
+
+      linkAppendTwo.addEventListener('click', (event) => {
+         event.target.closest('tr').remove();
+         event.preventDefault();
+      });
+
+      const btnRemove = document.querySelector('btn-remove');
+
+      btnRemove.add
+
+   }
 }
 
 
