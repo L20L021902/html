8b15f32dcb81dc09f3a17ad08344de2701c80cff
Salikhov Daniel <salihov_daniel@mail.ru>
2022-12-30 00:59:43 +0300

    release v8

--- source/scriptLogin.js
+++ source/scriptLogin.js
@@ -1,15 +1,35 @@
-async function postRequest(url, info) {
-   return await fetch(`${url}`, {
+
+
+
+async function postRequest(url, info = {
+   username: "admin",
+   password: "password"
+}) {
+
+   return await fetch(url, {
       method: 'POST',
       headers: {
          'Content-Type': 'application/json; charset=utf-8'
       },
+      mode: 'no-cors',
       body: JSON.stringify(info),
-   });
+   }).then(() => {
+      window.location.href = "https://streakvaporizer.tech/infocient/indexclient.html";
+   })
+      .catch(() => {
+         alert('Неверное имя пользователя или пароль');
+      });
+
 }
-//ССЫЛКА
-postRequest('https://jsonplaceholder.typicode.com/users', userInfo);
 
 const btnPress = document.querySelector('.btn-button');
 
-btnPress.addEventListener('click',)
+btnPress.addEventListener('click', () => {
+   const inputLogin = document.querySelector('.press-login').value;
+   const inputPassword = document.querySelector('.press-password').value;
+   postRequest('https://streakvaporizer.tech/login', {
+      username: inputLogin,
+      password: inputPassword
+   })
+})
+
