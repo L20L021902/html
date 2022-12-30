cfa38306e5f02f45879e1288c201e0ea52810ae5
Salikhov Daniel <salihov_daniel@mail.ru>
2022-12-30 16:13:59 +0300

    release v11

---
+++ source/scriptRegistration.js
@@ -0,0 +1,29 @@
+async function postRequest(url, info) {
+
+   return await fetch(url, {
+      method: 'POST',
+      headers: {
+         'Content-Type': 'application/json; charset=utf-8'
+      },
+      mode: 'no-cors',
+      body: JSON.stringify(info),
+   }).then(() => {
+      window.location.href = "https://streakvaporizer.tech/infocient/indexclient.html";
+   })
+      .catch(() => {
+         alert('Такой пользователь уже существует');
+      });
+
+}
+
+const btnPress = document.querySelector('.btn-button');
+
+btnPress.addEventListener('click', (e) => {
+   e.preventDefault();
+   const inputLogin = document.querySelector('.press-login').value;
+   const inputPassword = document.querySelector('.press-password').value;
+   postRequest('http://streakvaporizer.tech/registration/register', {
+      username: inputLogin,
+      password: inputPassword
+   })
+})
