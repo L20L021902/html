f7d5097ae14d0b67a7205fcddad0100c8d85f664
L20L021902 <L20L021902@stu.hit.edu.cn>
2022-12-30 19:57:36 +0300

    implement index page update

--- source/scriptClient.js
+++ source/scriptClient.js
@@ -3,7 +3,6 @@ function findByClass(className) {
 }
 
 let buttonForm = findByClass('.button-send');
-let buttonClear = findByClass('.button-clear');
 
 async function postRequest(url, info) {
   return await fetch(`${url}`, {
@@ -12,49 +11,24 @@ async function postRequest(url, info) {
       'Content-Type': 'application/json; charset=utf-8'
     },
     body: JSON.stringify(info),
+  }).then(() => {
+	window.location.reload();
+  }).catch(() => {
+	alert("Couldn't update user info")
   });
 }
 
-buttonForm.addEventListener('click', () => {
-  let name = findByClass('.head-header1').childNodes[3].value;
+buttonForm.addEventListener('click', (e) => {
+	e.preventDefault();
+  let realname = findByClass('.head-header1').childNodes[3].value;
   let address = findByClass('.top1-wrap').childNodes[3].value;
   let phoneNumber = findByClass('.top2-wrap').childNodes[3].value;
   let sex = findByClass('.header2-sex').value;
 
-  let userInfo = {
-    name: name,
+  postRequest('/infocient/update', {
+    realname: realname,
     address: address,
-    number: phoneNumber,
+    phone: phoneNumber,
     sex: sex,
-  };
-  // ССЫЛКА
-  postRequest('https://jsonplaceholder.typicode.com/users', userInfo);
-  if (name == '') return;
-  else {
-    findByClass('.info-head').innerHTML = userInfo.name + ',';
-    findByClass('.header-menu').innerHTML = `${userInfo.name}, ` + '欢迎使用本系统';
-    findByClass('.icon-text').innerHTML = userInfo.name;
-    findByClass('.info-headbottom').innerHTML = `ID: ${Math.floor(Math.random() * 1000000)}`
-  };
-  clearFields();
-
-});
-
-function clearFields() {
-  findByClass('.head-header1').children[1].value = '';
-  findByClass('.top1-wrap').children[1].value = '';
-  findByClass('.top2-wrap').children[1].value = '';
-  findByClass('.header2-sex').children[0].selected = true;
-}
-
-buttonClear.addEventListener('click', () => {
-  clearFields();
+  })
 })
-
-
-
-
-
-
-
-
